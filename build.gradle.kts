8be5d7e547b4c77b20b01d8c1c471bc676233f11
luanyaolingwu <user3678169@163.com>
2024-07-03 12:28:52 +0800

    加点有的没的

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,6 @@ plugins {
     id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:unchecked"))
+}
