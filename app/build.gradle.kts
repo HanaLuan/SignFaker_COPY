2c70c6c395bf9e033f4ec3b43853f7352ad9b9b1
Eruru <1633756198@qq.com>
2024-12-10 17:32:57 +0800

    临时解决Android15 界面显示不全问题
    Fuck U Google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,7 +122,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-host-common:$ktor_version")
